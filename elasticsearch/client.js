ca86b66e0000822642ed4ffc1f124bb15f7de29a
Nikolov <bobinikolov33@gmail.com>
2021-04-25 14:21:30 +0300

    Updated client

--- elasticsearch/client.js
+++ elasticsearch/client.js
@@ -1,36 +1,55 @@
-
 exports.getClient = function() {
-    var client = new Client();
     var native = org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.getClient();
 
-    client.native = native;
-
-    return client;
+    return new Client(native);
 } 
 
-function Client() {
-    this.getDocumentApi = function() {
-        var documentApi = new DocumentApi();
+function Client(native) {
+    this.native = native;
 
-        documentApi.native = this.native;
+    this.documents = new DocumentApi(this.native);
 
-        return documentApi;
+    this.indexes = new IndexAPI(this.native);
+
+    this.close = function() {
+        this.native.close();
     }
 }
 
-function DocumentApi() {
-    this.index = function(index, id, documentSource) {
-        return org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.index(this.native, index, id, documentSource);
+function DocumentApi(native) {
+    this.native = native;
+
+    this.index = function(index, id, documentSource, xContentType = "JSON") {
+        return org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.indexDocument(this.native, index, id, documentSource, xContentType);
     }
 
     this.get = function(index, id) {
-        var document = org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.get(this.native, index, id);
+        var getResult = org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.getDocument(this.native, index, id);
 
-        return document.getSourceAsString();
+        return getResult.getSourceAsString();
+    }
+
+    this.exists = function(index, id) {
+        return org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.documentExists(this.native, index, id);
     }
 
     this.delete = function(index, id) {
-        return org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.delete(this.native, index, id);
+        return org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.deleteDocument(this.native, index, id);
     }
 }
 
+function IndexAPI(native) {
+    this.native = native;
+
+    this.create = function(name) {
+        return org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.createIndex(this.native, name);
+    }
+
+    this.delete = function(name) {
+        return org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.deleteIndex(this.native, name);
+    }
+
+    this.exists = function(name) {
+        return org.eclipse.dirigible.api.elasticsearch.ElasticsearchFacade.indexExists(this.native, name);
+    }
+}
